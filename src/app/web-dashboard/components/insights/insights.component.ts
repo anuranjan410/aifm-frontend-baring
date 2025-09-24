3f9d505885651b7b997c5f6e86c09f3f37035dc3
Anuranjan410 <anuranjan410@gmail.com>
2025-09-24 23:22:04 +0530

    fix for the insights

--- src/app/web-dashboard/components/insights/insights.component.ts
+++ src/app/web-dashboard/components/insights/insights.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,35 +9,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./insights.component.scss']
 })
 export class InsightsComponent {
-  insights = [
-    {
-      title: 'Future of Digital Transformation in Enterprise',
-      image: 'https://api.builder.io/api/v1/image/assets/TEMP/3936939ad970f7a9b459a32d0e26039e89d24225?width=620',
-      category: 'Analysis',
-      readTime: '5 min read',
-      date: 'Oct 15, 2024'
-    },
-    {
-      title: 'Achieving sustainable growth: Make sustainability the focus',
-      image: 'https://api.builder.io/api/v1/image/assets/TEMP/3936939ad970f7a9b459a32d0e26039e89d24225?width=620',
-      category: 'Interview',
-      readTime: '8 min read',
-      date: 'Oct 12, 2024'
-    },
-    {
-      title: 'An attractive time to get more aggressive: Baring India',
-      image: 'https://api.builder.io/api/v1/image/assets/TEMP/3936939ad970f7a9b459a32d0e26039e89d24225?width=620',
-      category: 'Analysis',
-      readTime: '6 min read',
-      date: 'Oct 8, 2024'
-    }
-  ];
-
+  
   onViewAllInsights() {
     console.log('Navigate to insights page');
   }
 
-  onReadInsight(insight: any) {
-    console.log('Read insight:', insight.title);
+  onReadInsight(type: string) {
+    console.log('Read insight:', type);
   }
 }
